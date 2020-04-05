4f4420f1e501fe4b4dd1464e4324840720317926
root <root@icp1.bpt.com>
2020-04-05 08:40:45 +0700

    test kabanero local

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody Ya 2 !!!!!");
+  res.send("Hello from Appsody Ya local !!!!!");
 });
  
 module.exports.app = app;
