3f1e496aaebc228bbbad4005b0e7af2bf6db2674
root <farizalandrian@yahoo.co.id>
2020-04-05 08:57:40 +0700

    test kabanero local 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody Ya local !!!!!");
+  res.send("Hello from Appsody Ya local 05-04 !!!!!");
 });
  
 module.exports.app = app;
